3f348d97ed9e84fd718a81163dc0ea73a7ef7726
Phil Ngan <4557674+pngan@users.noreply.github.com>
2024-12-18 07:29:03 +1300

    Refactored Part 1 to include State

--- AdventOfCode.Solutions/Days/day16.cs
+++ AdventOfCode.Solutions/Days/day16.cs
@@ -8,6 +8,7 @@ using AdventureOfCode.Utilities.Image;
 namespace AdventOfCode.Solutions.Days;
 
 using Point = (int r, int c);
+using State = ((int r, int c) p, (int dr, int dc) s);
 
 public class Day16 : BaseDay<(CharImage2 map, Point start, Point end)>
 {
@@ -23,53 +24,53 @@ public class Day16 : BaseDay<(CharImage2 map, Point start, Point end)>
 
     protected override object Solve1((CharImage2 map, Point start, Point end) input)
     {
-        Dictionary<(Point, (int dr, int dc), Point, (int dr, int dc)), ulong> costs = new();
-        var pq = new PriorityQueue<(Point, (int,int), Point, (int,int)), ulong>();
-        pq.Enqueue((input.start, Step.E, input.start, Step.E), 0);
-        Dictionary<(Point p, (int dr, int dc) s), (Point p, (int dr, int dc) s)> visited = new(); // <Point, Previous Point>
+        Dictionary<(State, State), ulong> costs = new();
+        var pq = new PriorityQueue<(State curr, State prev), ulong>();
+        pq.Enqueue(((input.start, Step.E), (input.start, Step.E)), 0);
+        Dictionary<State, State> visited = new(); // <Point, Previous Point>
 
         while (true)
         {
             // Stop if priority queue is empty
-            if (!pq.TryDequeue(out (Point p, (int dr, int dc) s, Point prevPoint, (int dr, int dc) prevStep) curr, out ulong cost) )
+            if (!pq.TryDequeue(out (State curr, State prev) best, out ulong cost) )
                 break;
 
             // Ignore if the inspected node has already been visited
-            if (visited.ContainsKey((curr.p, curr.s)))
+            if (visited.ContainsKey((best.curr)))
                 continue;
 
             // Handle minimum item from priority queue
-            costs[curr] = cost;
-            visited[(curr.p, curr.s)] = (curr.prevPoint, curr.prevStep);
+            costs[best] = cost;
+            visited[best.curr] = best.prev;
 
             HashSet<Point> uniqueLocations = new();
-            if (curr.p == input.end)
+            if (best.curr.p == input.end)
             {
-                var prev = visited[(curr.p, curr.s)];
-                uniqueLocations.Add(curr.p);
+                var prev = visited[best.curr];
+                uniqueLocations.Add(best.prev.p);
                 while (prev != (input.start, Step.E))
                 {
                     uniqueLocations.Add((prev.p));
                     prev = visited[(prev)];
                 }
-                Console.WriteLine(uniqueLocations.Count-1);
+                Console.WriteLine(uniqueLocations.Count);
                 return cost;
             }
             // Next possible actions according to puzzle:
 
             // Walk one step in current direction
-            var step0 = curr.s.Rot0();
-            var p0 = curr.p.Add(step0);
+            var step0 = best.curr.s.Rot0();
+            var p0 = best.curr.p.Add(step0);
             if (input.map[p0] != '#' && InBounds(p0))
-                pq.Enqueue((p0, step0, curr.p, curr.s), cost + 1);
+                pq.Enqueue(((p0, step0), best.curr), cost + 1);
 
             // Turn 90 deg
-            var step90 = curr.s.Rot90();
-            pq.Enqueue((curr.p, step90, curr.p, curr.s), cost + 1000);
+            var step90 = best.curr.s.Rot90();
+            pq.Enqueue(((best.curr.p, step90), best.curr), cost + 1000);
 
             // Turn 270 deg
-            var step270 = curr.s.Rot270();
-            pq.Enqueue((curr.p, step270, curr.p, curr.s), cost + 1000);
+            var step270 = best.curr.s.Rot270();
+            pq.Enqueue(((best.curr.p, step270), best.curr), cost + 1000);
         }
 
         bool InBounds(Point p) => p.r >= 0 && p.c >= 0 && p.r < input.map.ROWS && p.c < input.map.COLS;
