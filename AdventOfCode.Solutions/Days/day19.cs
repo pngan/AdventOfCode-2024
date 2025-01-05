86e2656bc30dee4f059fe32931f51c3e5935180a
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-01-05 22:45:57 +1300

    Tidyup Day 19

--- AdventOfCode.Solutions/Days/day19.cs
+++ AdventOfCode.Solutions/Days/day19.cs
@@ -16,37 +16,24 @@ public class Day19 : BaseDay<(IEnumerable<string> towels, IEnumerable<string> pa
     protected override object Solve1((IEnumerable<string> towels, IEnumerable<string> patterns) input)
     {
         int count = 0;
-        Dictionary<string, ulong> memo = new(); // One memo for all patterns to avoid recalculating
+        Dictionary<string, long> memo = new(); // One memo for all patterns to avoid recalculating
         foreach (var pattern in input.patterns)
             count += Parse(pattern, input.towels.ToArray(), memo) > 0 ? 1 : 0;
         return count;
     }
 
-    protected override object Solve2((IEnumerable<string> towels, IEnumerable<string> patterns) input)
-    {
-        ulong count = 0;
-        Dictionary<string, ulong> memo = new(); // One memo for all patterns to avoid recalculating
-        foreach (var pattern in input.patterns)
-            count += Parse(pattern, input.towels.ToArray(), memo);
-        return count;
-    }
+    protected override object Solve2((IEnumerable<string> towels, IEnumerable<string> patterns) input) 
+        => input.patterns.Sum(pattern => Parse(pattern, input.towels.ToArray(), new Dictionary<string, long>()));
 
-    private ulong Parse(string pattern, string[] towels, Dictionary<string, ulong> count)
+    private long Parse(string pattern, string[] towels, Dictionary<string, long> count)
     {
         if (count.ContainsKey(pattern))
             return count[pattern];
         if (pattern.Length == 0)
             return 1;
-        ulong total = 0;
-        foreach (var towel in towels)
-        {
-            if (pattern.StartsWith(towel))
-            {
-                var residual = pattern[towel.Length..];
-                total += Parse(residual, towels, count);
-            }
-        }
-        count[pattern] = total;
-        return total;
+        count[pattern] = towels
+            .Where(pattern.StartsWith)
+            .Sum(towel => Parse(pattern[towel.Length..], towels, count));
+        return count[pattern];
     }
 }
