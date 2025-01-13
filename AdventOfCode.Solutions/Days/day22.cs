d22dc0d15584ba7ce051c81221256245315b29ac
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-01-13 22:05:25 +1300

    [2024 Day 22]

---
+++ AdventOfCode.Solutions/Days/day22.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Immutable;
+
+using AdventOfCode.Solutions.Common;
+using AdventOfCode.Solutions.Extensions;
+
+using System.Numerics;
+
+namespace AdventOfCode.Solutions.Days;
+
+public class Day22 : BaseDay<IEnumerable<ulong>>
+{
+    protected override int DayNumber => 22;
+
+    const int Iterations = 2000;
+
+    protected override IEnumerable<ulong> Parse(ImmutableArray<string> input) => input.Select(Convert.ToUInt64);
+
+    protected override object Solve1(IEnumerable<ulong> input)
+    {
+        ulong result = 0;
+        
+        foreach (var i in input)
+        {
+            var secret = i;
+            for (int j = 0; j < Iterations; j++)
+            {
+                secret = Iterate(secret);
+            }
+            result += secret;
+        }
+        return result;
+    }
+
+    protected override object Solve2(IEnumerable<ulong> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static ulong Iterate(ulong i)
+    {
+        i ^= i << 6;
+        i &= 0xffffff;
+        i ^= i >> 5;
+        i &= 0xffffff;
+        i ^= i << 11;
+        i &= 0xffffff;
+
+        return i;
+    }
+}
+
+public static class I64Extensions
+{
+    public static ulong Mix(this ulong input, ulong value) => input ^ value;
+    public static ulong Prune(this ulong input) =>  input & 0xffffff;  // fast modulo 16777216
+    public static ulong Mult64(this ulong input) =>  input << 6;
+    public static ulong Div32(this ulong input) => input >> 5; 
+    public static ulong Mult2048(this ulong input) => input << 11;
+
+
+}
+
+
