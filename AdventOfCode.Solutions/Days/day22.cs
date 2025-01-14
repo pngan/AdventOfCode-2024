fedd8475ef197459b5ca9f0f4c1f53728e017dc2
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-01-14 22:05:15 +1300

    Add CircularBuffer

--- AdventOfCode.Solutions/Days/day22.cs
+++ AdventOfCode.Solutions/Days/day22.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Immutable;
 
 using AdventOfCode.Solutions.Common;
-using AdventOfCode.Solutions.Extensions;
-
-using System.Numerics;
 
 namespace AdventOfCode.Solutions.Days;
 
@@ -18,7 +15,7 @@ public class Day22 : BaseDay<IEnumerable<ulong>>
     protected override object Solve1(IEnumerable<ulong> input)
     {
         ulong result = 0;
-        
+
         foreach (var i in input)
         {
             var secret = i;
@@ -48,16 +45,40 @@ public class Day22 : BaseDay<IEnumerable<ulong>>
         return i;
     }
 }
-
-public static class I64Extensions
+public class CircularQuadBuffer
 {
-    public static ulong Mix(this ulong input, ulong value) => input ^ value;
-    public static ulong Prune(this ulong input) =>  input & 0xffffff;  // fast modulo 16777216
-    public static ulong Mult64(this ulong input) =>  input << 6;
-    public static ulong Div32(this ulong input) => input >> 5; 
-    public static ulong Mult2048(this ulong input) => input << 11;
+    private const int SIZE = 5;
+    private readonly int[] _buffer = new int[SIZE];
+    private int _p = -1;
+    private int _pushes = 0;
 
+    public void Push(ulong item)
+    {
+        _pushes++;
+        _p++;
+        if (_p == SIZE) _p = 0;
+        _buffer[_p] = (int)item % 10;
+    }
+    public int Current()
+    {
+        if (_pushes < SIZE) throw new IndexOutOfRangeException();
+        return _buffer[_p];
+    }
+    private int[] Values()
+    {
+        if (_pushes < SIZE) throw new IndexOutOfRangeException();
+        int[] result = new int[SIZE];
+        result[4] = _buffer[_p];
+        result[3] = _buffer[(_p + SIZE - 1) % SIZE];
+        result[2] = _buffer[(_p + SIZE - 2) % SIZE];
+        result[1] = _buffer[(_p + SIZE - 3) % SIZE];
+        result[0] = _buffer[(_p + SIZE - 4) % SIZE];
 
+        return result;
+    }
+    public (int, int, int, int) Changes()
+    {
+        var v = Values();
+        return (v[1] - v[0], v[2] - v[1], v[3] - v[2], v[4] - v[3]);
+    }
 }
-
-
